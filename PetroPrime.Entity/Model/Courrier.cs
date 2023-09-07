08dec36441de82d45faaaa4490e22f1f232c3d24
Nayan Gupta <kanumgupta32@gmail.com>
2023-09-07 05:53:31 +0000

    Created Model

---
+++ PetroPrime.Entity/Model/Courrier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetroPrime.Entity.Model
+{
+    public class Courrier
+    {
+        public int CourrierId { get; set; }
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ PetroPrime.Entity/Model/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetroPrime.Entity.Model
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDetails { get; set; }
+        public int SupplierId { get; set; }
+        public int CategoryId { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+    }
+}
